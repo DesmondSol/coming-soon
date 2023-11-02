e17cfce127e33e1950df184c4c06273ecf9cb3d7
Sol Tig <51263712+DesmondSol@users.noreply.github.com>
2023-11-02 18:54:03 +0300

    Update App.js
    just modified it a bit to suit our needs on social links needs to be improved upon in a bit

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">Trendy Merches</a>
           </div>
-          <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+          <div className="social"> 
+            <a href="https://liyu.vercel.app/" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+            <a href="https://liyu.vercel.app/" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+            <a href="https://liyu.vercel.app/" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
             </a>
           </div>
@@ -34,7 +34,7 @@ class App extends React.Component {
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          <span>made by <a className="underlined" href="https://liyu.vercel.app/" target="_blank" rel="noopener noreferrer">Solomon T</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://liyu.vercel.app/" title="GitHub repo" target="_blank" rel="noopener noreferrer">website</a></span>
         </div>
       </div>
     );
